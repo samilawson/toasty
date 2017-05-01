e17bd9b93adccf9a57e9eb5fdd210ee713be3e0c
Gavin Wainwright <gav.wainwright@gmail.com>
2017-05-01 19:13:58 -0400

    Launched shard event

--- src/launcher.js
+++ src/launcher.js
@@ -1,4 +1,5 @@
 "use strict";
 const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager(`${__dirname}/toasty.js`, {totalShards: 2});
+Manager.on('launch', shard => console.log(`Launched ${shard.id}`));
 Manager.spawn().catch(console.error);
