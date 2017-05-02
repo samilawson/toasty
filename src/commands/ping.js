9d8e3a0832c97c82b952ec23bbbbe16e66bdbb00
Gavin Wainwright <gav.wainwright@gmail.com>
2017-05-02 16:00:37 -0400

    ping to google

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,9 +1,12 @@
-const now = require('performance-now');
+const now = require('performance-now'),
+      ping = require('async-pinger');
 exports.run = (client, msg) => {
     let start = now();
     msg.channel.sendMessage("*Pinging...*")
         .then(m => {
             let end = now();
-            m.edit(`:ping_pong: Pong!\nLatency by timestamp: **${m.createdTimestamp - msg.createdTimestamp}ms**\nLatency by now(): **${(end - start).toFixed(0)}ms**\nDiscord Latency: **${Math.round(client.ping)}ms**`);
+            let googlePing = new Array();
+            ping('google.com', (err, ms) => { googlePing.push(ms) });
+            m.edit(`:ping_pong: Pong!\nLatency by timestamp: **${m.createdTimestamp - msg.createdTimestamp}ms**\nLatency by now(): **${(end - start).toFixed(0)}ms**\nDiscord Latency: **${Math.round(client.ping)}ms**\nGoogle latency: **${googlePing[0]}**ms`);
         });
 }
