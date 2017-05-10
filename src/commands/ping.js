cf317ded8d00b8ec009a4e3962afff9d11eb8a34
Gavin Wainwright <gav.wainwright@gmail.com>
2017-05-09 20:02:25 -0400

    Website ping + reply w/ error

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,9 +5,9 @@ exports.run = (client, msg) => {
     msg.channel.sendMessage("*Pinging...*")
         .then(m => {
             let end = now();
-            ping('google.com', (err, ms) => {
-                if (err) console.error(err);
-                m.edit(`:ping_pong: **Pong!**\nMessage latency: **${m.createdTimestamp - msg.createdTimestamp}ms**\nEdit latency: **${(end - start).toFixed(0)}ms**\nDiscord Latency: **${Math.round(client.ping)}ms**\nGoogle latency: **${ms}ms**`);
+            ping('toastythebot.tk', (err, ms) => {
+                if (err) msg.reply('**Error:** ' + err);
+                m.edit(`:ping_pong: **Pong!**\nMessage latency: **${m.createdTimestamp - msg.createdTimestamp}ms**\nEdit latency: **${(end - start).toFixed(0)}ms**\nDiscord Latency: **${Math.round(client.ping)}ms**\nWebsite latency: **${ms}ms**`);
           });
         });
 }
