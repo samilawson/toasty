2ea7c9438669f1645946090b64d09405595dcef2
gavwin <minerdude010@gmail.com>
2017-05-25 18:21:11 -0400

    additions and fixes

---
+++ web/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
+
+http.listen(80, () => {
+  console.log('toastythebot.tk is now ready.');
+});
+
+app.use(express.static(`${__dirname}/static`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/features', (req, res) => {
+  res.sendFile(`${__dirname}/features.html`);
+});
+
+app.get('/hq', (req, res) => {
+  res.sendFile(`${__dirname}/hq.html`);
+});
+
+app.get('/invite', (req, res) => {
+  res.sendFile(`${__dirname}/invite.html`);
+});
+
+app.get('/apply', (req, res) => {
+  res.sendFile(`${__dirname}/apply.html`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('stats', () => {
+    let stats = JSON.parse(fs.readFileSync(`${__dirname}/stats.json`));
+    io.emit('stats', stats);
+  });
+});
