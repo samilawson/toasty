322b3bc43e7cd9e8e60b192ded74637e65ffa56a
Gavin Wainwright <gav.wainwright@gmail.com>
2017-05-26 09:15:57 -0400

    unnecessary "use strict"

--- src/launcher.js
+++ src/launcher.js
@@ -1,4 +1,3 @@
-"use strict";
 const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager(`${__dirname}/toasty.js`, {totalShards: 2});
 Manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
